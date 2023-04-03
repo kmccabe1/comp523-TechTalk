d51407941aca4090cbbbf0fa3d3ce78eb1f3d501
Kevin McCabe <kmccabe1@email.unc.edu>
2023-04-02 21:46:00 -0400

    Added options to command

--- click_demo.py
+++ click_demo.py
@@ -1,9 +1,17 @@
 import click
 
 @click.command()
-def say_goodbye():
-    click.echo("Goodbye")
+# Options can be given default values and custom help messages.
+@click.option("--count", "-c", default=1, help="Number of goodbyes.")
+# Prompt will ask the user for input for the option.
+@click.option("--name", prompt="Your name", help="The person to say goodbye to.")
+def say_goodbye(count, name):
+    # Add a docstring that will be used by the --help option.
+    """Say goodbye to the user."""
+    for i in range(count):
+        click.echo(f"Goodbye {name}.")
 
 
 if __name__=="__main__":
     say_goodbye()
+
